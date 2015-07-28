7f53ac8fae69f16a0c6a7c17ea7eeb795d65abce
Kasper Munch <kaspermunch@birc.au.dk>
2015-07-28 11:42:40 +0200

    Changed setup.py to work without ez_setup

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
+#!/usr/bin/env python
 
-import ez_setup
-ez_setup.use_setuptools()
+from distutils.core import setup
 
-import sys, glob
-from setuptools import setup, Extension, find_packages
-
-if sys.version < '2.7':
-    print "This package requires Python version 2.7 or higher. Aborting."
-    print "Visit http://www.python.org/download for a more recent version of Python."
-    print "Aborting."
-    sys.exit()
-
-version = "1.0.0"
-package_name = "SimulationPipeline"
-
-setup(name=package_name,
-      test_suite='tests',
-      version=version,
+setup(name='SimulationPipeline',
+      version='1.0',
       description='',
-      long_description='',
       author='Kasper Munch',
       author_email='kaspermunch@birc.au.dk',
-      url='http://users-cs.au.dk/kmt',
-      packages = find_packages(exclude=['ez_setup']),
-      package_dir = {package_name: package_name},
-      include_package_data = True,
-      entry_points = { 'console_scripts': [ 'run_simulation_pipeline = SimulationPipeline.Utils:runSimulationsWithCoaSimScript',
-                                            'run_simulation_pipeline_with_ctmc = SimulationPipeline.Utils:runSimulationsWithCoaSimAndILSCTMCScript', ],
-                       },
-      )
+      packages=['SimulationPipeline'],
+     )
