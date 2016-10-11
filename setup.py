61b4c3715166cc99ed035a646d462566a1726936
Kasper Munch <kaspermunch@birc.au.dk>
2016-10-11 13:06:30 +0200

    changed from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+#from distutils.core import setup
 
 setup(name='SimulationPipeline',
       version='1.0',
       description='',
       author='Kasper Munch',
       author_email='kaspermunch@birc.au.dk',
-      packages=['SimulationPipeline'],
+      #packages=['SimulationPipeline'],
+      packages=find_packages(),
 	  entry_points = { 'console_scripts': [ 'run_simulation_pipeline = SimulationPipeline.Utils:runSimulationsWithCoaSimAndILS09Script',
                                             'run_ils16_simulation_pipeline = SimulationPipeline.Utils:runSimulationsWithCoaSimAndILS16Script',
                                             'run_simulation_pipeline_with_ctmc = SimulationPipeline.Utils:runSimulationsWithCoaSimAndILSCTMCScript', ]
